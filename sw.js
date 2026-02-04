23a6644f7ede34e46348e711501e326278099e64
shancollectives <huzaifaaa.028@gmail.com>
2026-02-04 16:50:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'salah-cache-v1';
+const CACHE_NAME = 'salah-v1';
 const OFFLINE_URL = 'index.html';
 
 self.addEventListener('install', (event) => {
@@ -15,19 +15,9 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          return cache.match(OFFLINE_URL);
-        });
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  }
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(OFFLINE_URL);
+    })
+  );
 });
