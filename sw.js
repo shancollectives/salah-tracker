16fc521360a8bf987254cfe7c9506292ff51fedf
shancollectives <huzaifaaa.028@gmail.com>
2026-02-04 15:49:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
 const CACHE_NAME = 'salah-tracker-v1';
 const OFFLINE_URL = 'index.html';
 
+// Install: Cache the offline page
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // This tells the browser to cache your main page
       return cache.addAll([OFFLINE_URL]);
     })
   );
+  self.skipWaiting();
 });
 
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Fetch: Serve the offline page when there is no internet
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith(
