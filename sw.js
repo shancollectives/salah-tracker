e6ee67478a7e2a2008e8a334b47c6e4ac2a6d150
shancollectives <huzaifaaa.028@gmail.com>
2026-02-04 16:59:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'salah-v1';
-const OFFLINE_URL = 'index.html';
+const CACHE_NAME = "pwabuilder-offline";
+const QUEUE_NAME = "bgSyncQueue";
+const offlineFallbackPage = "index.html";
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([OFFLINE_URL]);
+      return cache.add(offlineFallbackPage);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(OFFLINE_URL);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          return cache.match(offlineFallbackPage);
+        });
+      })
+    );
+  }
 });
