3b4622ca38f4fbe663682067bab72d8353cd88a0
shancollectives <huzaifaaa.028@gmail.com>
2026-02-04 17:28:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,31 @@
-const CACHE_NAME = 'offline-v1';
-const FILES_TO_CACHE = [
+const CACHE_NAME = 'salah-cache-v1';
+const ASSETS = [
+  'index.html',
   './',
-  'index.html'
+  'manifest.json',
+  'sw.js'
 ];
 
+// Install: Save all files to the cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
+  self.skipWaiting();
 });
 
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Fetch: Serve from cache even if offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
